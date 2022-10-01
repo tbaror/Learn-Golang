e1aff7770e6ba8209aa9649c78f1f360599e7b79
tbaror <tal_baror@hotmail.com>
2022-10-01 13:40:32 +0300

    progress json

---
+++ json-example/exjaon.go
@@ -0,0 +1,31 @@
+package main
+
+import ( "encoding/json"
+         "fmt"
+         "log"
+)
+
+
+
+
+
+
+
+
+func main() {
+	myJson := `
+[
+    {
+        "first_name": "Clark",
+        "last_name": "Kent",
+        "hair_color": "black",
+        "has_dog": true
+    },
+    {
+        "first_name": "Bruce",
+        "last_name": "Wayne",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
+}
